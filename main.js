d90ea120652bcefbbbb6d956f3dee63832fb2679
Zang MingJie <zealot0630@gmail.com>
2019-08-03 22:01:18 +0800

    修正大会员限制，添加国际友人看国内功能

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name               Bilibili 港澳台
 // @namespace          http://kghost.info/
-// @version            0.9
+// @version            1.0
 // @description:       Remove area restriction
-// @description:zh-CN  解除区域限制
+// @description:zh-CN  解除区域限制(修正大会员限制，添加国际友人看国内功能)
 // @supportURL         https://github.com/kghost/bilibili-area-limit
 // @author             zealot0630
-// @include            https://www.bilibili.com/*
+// @include            https://*.bilibili.com/*
 // @run-at document-start
 // @description Bilibili 港澳台, 解除区域限制
+// @grant       GM_cookie
 // ==/UserScript==
 
 const url_status = [
@@ -17,19 +18,39 @@ const url_status = [
 ];
 const url_play = /^https:\/\/api\.bilibili\.com\/pgc\/player\/web\/playurl\?.*/;
 
-const url_replace_www = /^https:\/\/www\.bilibili\.com\//;
-const url_replace_www_to = {
-  'hk': 'https://bilibili-hk-www.kghost.info/',
-  'tw': 'https://bilibili-tw-www.kghost.info/',
-};
-
-const url_replace = /^https:\/\/api\.bilibili\.com\//;
+const url_api_replace = /^https:\/\/api\.bilibili\.com\//;
+const url_www_replace = /^https:\/\/www\.bilibili\.com\//;
 const url_replace_to = [
-  [/僅.*港.*地區/, 'https://bilibili-hk-api.kghost.info/'],
-  [/僅.*台.*地區/, 'https://bilibili-tw-api.kghost.info/'],
+  [
+    // HK
+    /僅.*港.*地區/,
+    {
+      www: 'https://bilibili-hk-www.kghost.info/',
+      api: 'https://bilibili-hk-api.kghost.info/',
+      info: 'https://bilibili-hk-info.kghost.info/',
+    },
+  ],
+  [
+    // TW
+    /僅.*台.*地區/,
+    {
+      www: 'https://bilibili-tw-www.kghost.info/',
+      api: 'https://bilibili-tw-api.kghost.info/',
+      info: 'https://bilibili-tw-info.kghost.info/',
+    },
+  ],
+  [
+    // CN
+    /^((?!僅).)*$/,
+    {
+      www: 'https://bilibili-cn-www.kghost.info/',
+      api: 'https://bilibili-cn-api.kghost.info/',
+      info: 'https://bilibili-cn-info.kghost.info/',
+    },
+  ],
 ];
 
-(function (XMLHttpRequest) {
+(function(XMLHttpRequest) {
   class ClassHandler {
     constructor(proxy) {
       this.proxy = proxy;
@@ -46,7 +67,6 @@ const url_replace_to = [
   class ObjectHandler {
     constructor(target) {
       this.target = target;
-      this.log = false;
     }
 
     get(target, prop, receiver) {
@@ -58,7 +78,8 @@ const url_replace_to = [
         return this;
       } else {
         const value = target[prop];
-        if (typeof value == 'function') return new Proxy(value, new FunctionHandler(value));
+        if (typeof value == 'function')
+          return new Proxy(value, new FunctionHandler(value));
         return value;
       }
     }
@@ -89,7 +110,8 @@ const url_replace_to = [
     }
 
     getListeners(event) {
-      if (!this.listeners.hasOwnProperty(event)) this.listeners[event] = new Map();
+      if (!this.listeners.hasOwnProperty(event))
+        this.listeners[event] = new Map();
       return this.listeners[event];
     }
 
@@ -143,10 +165,10 @@ const url_replace_to = [
       if (prop === 'open') {
         return new Proxy(target.open, new this.open(target.open));
       } else if (prop === 'response' && this.overrideResponse) {
-        console.log("BAL: Return hooked area limit")
+        console.log('BAL: Return hooked area limit');
         return this.overrideResponseValue;
       } else if (prop === 'responseText' && this.overrideResponse) {
-        console.log("BAL: Return hooked area limit")
+        console.log('BAL: Return hooked area limit');
         return this.overrideResponseValue;
       } else {
         return super.get(target, prop, receiver);
@@ -164,23 +186,25 @@ const url_replace_to = [
         if (limited && url.match(url_play)) {
           for (const [match, to] of url_replace_to) {
             if (document.title.match(match)) {
-              argumentsList[1] = url.replace(url_replace, to);
-              console.log(`BAL: playurl via proxy ${to}.`)
+              argumentsList[1] = url.replace(url_api_replace, to.api);
+              console.log(`BAL: playurl via proxy ${to.api}.`);
               break;
             }
           }
-        } else if ((function () {
+        } else if (
+          (function() {
             for (const status of url_status) {
               if (url.match(status)) return true;
             }
-          })()) {
+          })()
+        ) {
           realTarget.addEventListener('readystatechange', () => {
             if (realTarget.readyState === 4 && realTarget.status === 200) {
               const status = JSON.parse(realTarget.response);
               if (status && status.result && status.result.area_limit === 1) {
                 status.result.area_limit = 0;
                 limited = true;
-                console.log("BAL: Hook area limit")
+                console.log('BAL: Hook area limit');
                 proxy[ProxyGetHandler].overrideResponse = true;
                 proxy[ProxyGetHandler].overrideResponseValue = JSON.stringify(
                   status
@@ -195,22 +219,83 @@ const url_replace_to = [
   };
 
   unsafeWindow.XMLHttpRequest = new Proxy(
-    XMLHttpRequest, new ClassHandler(XhrHandler)
+    XMLHttpRequest,
+    new ClassHandler(XhrHandler)
   );
+
+  (() => {
+    var info = undefined;
+    var fetching = false;
+    Object.defineProperty(unsafeWindow, '__playinfo__', {
+      configurable: true,
+      get: function() {
+        if (info) return info;
+        console.log('BAL: Hook playinfo.');
+        const key = '__playinfo__' + window.location.href;
+        const cachedInfo = sessionStorage.getItem(key);
+        if (cachedInfo) return (info = JSON.parse(cachedInfo));
+        if (fetching) return undefined;
+        fetching = true;
+        console.log('BAL: Fetching playinfo.');
+
+        for (const [match, to] of url_replace_to) {
+          if (document.title.match(match)) {
+            GM_cookie.list(
+              { domain: '.bilibili.com', name: 'SESSDATA' },
+              (cookies, error) => {
+                if (error) {
+                  console.log('BAL: Error fetch info, not login');
+                  return;
+                }
+                const target = window.location.href.replace(
+                  url_www_replace,
+                  to.info
+                );
+                const xhr = new XMLHttpRequest();
+                xhr.open('GET', target);
+                xhr.responseType = 'document';
+                xhr.setRequestHeader('X-Cookie', cookies[0].value);
+                xhr.onreadystatechange = function() {
+                  if (this.readyState === xhr.DONE && this.status === 200) {
+                    console.log('BAL: Info fetched ...');
+                    for (const s of this.response.getElementsByTagName(
+                      'script'
+                    )) {
+                      if (s.innerHTML.includes('__playinfo__')) {
+                        eval(s.innerHTML);
+                        sessionStorage.setItem(
+                          key,
+                          JSON.stringify(window.__playinfo__)
+                        );
+                        console.log('BAL: Info fetched, reload page');
+                        window.location.reload();
+                      }
+                    }
+                  }
+                };
+                xhr.send();
+              }
+            );
+            break;
+          }
+        }
+        return undefined;
+      },
+      set: v => (info = v),
+    });
+  })();
 
   window.addEventListener('load', () => {
     if (document.querySelector('div.error-body')) {
       // try load via proxy
-      console.log("BAL: Load failed, try use proxy")
-      for (const loc in url_replace_www_to) {
+      console.log('BAL: Load failed, try use proxy');
+      for (const [u, loc] of url_replace_to) {
         const xhr = new XMLHttpRequest();
-        const url = window.location.href.replace(
-          url_replace_www, url_replace_www_to[loc]
-        );
+        const url = window.location.href.replace(url_www_replace, loc.www);
         xhr.open('HEAD', url);
-        xhr.onreadystatechange = function () {
+        xhr.onreadystatechange = function() {
           if (this.readyState === xhr.DONE && this.status === 204) {
-            console.log(`BAL: Redirected to ${loc}.`)
+            console.log(`BAL: Redirected to ${loc.www}.`);
             window.location = xhr.getResponseHeader('X-Location');
           }
         };
