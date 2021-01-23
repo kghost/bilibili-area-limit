7471387018bd145a67cb37d029723b1e47f6b35c
Zang MingJie <zealot0630@gmail.com>
2021-01-23 20:26:10 +0800

    Fix updates

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { url_status, url_play, url_api_replace, url_replace_to } from './url';
+import { url_play, url_api_replace, url_replace_to } from './url';
 
 (function (XMLHttpRequest) {
   class ClassHandler {
@@ -8,17 +8,13 @@ import { url_status, url_play, url_api_replace, url_replace_to } from './url';
 
     construct(target, args) {
       const obj = new target(...args);
-      return new Proxy(obj, new this.proxy(obj));
+      return new Proxy(obj, new this.proxy());
     }
   }
 
   const ProxyGetTarget = Symbol('ProxyGetTarget');
   const ProxyGetHandler = Symbol('ProxyGetHandler');
   class ObjectHandler {
-    constructor(target) {
-      this.target = target;
-    }
-
     get(target, prop, receiver) {
       if (target.hasOwnProperty(prop)) {
         return Reflect.get(target, prop, receiver);
@@ -28,71 +24,67 @@ import { url_status, url_play, url_api_replace, url_replace_to } from './url';
         return this;
       } else {
         const value = target[prop];
-        if (typeof value == 'function')
-          return new Proxy(value, new FunctionHandler(value));
+        if (typeof value == 'function') return new Proxy(value, new NativeFunctionHandler());
         return value;
       }
     }
 
-    set(target, prop, value) {
+    set(target, prop, value, receiver) {
       return Reflect.set(target, prop, value);
     }
   }
 
-  class FunctionHandlerBase extends ObjectHandler {
+  class FunctionHandler extends ObjectHandler {
     apply(target, thisArg, argumentsList) {
-      const realTarget = thisArg[ProxyGetTarget];
-      if (!realTarget) throw new Error('illegal invocations');
-      return this.call(this.target, thisArg, realTarget, argumentsList);
+      const realThis = thisArg[ProxyGetTarget];
+      if (!realThis) throw new Error('illegal invocations');
+      return this.call(target, thisArg, realThis, argumentsList);
     }
   }
 
-  class FunctionHandler extends FunctionHandlerBase {
-    call(fn, proxy, target, argumentsList) {
-      return fn.apply(target, argumentsList);
+  class NativeFunctionHandler extends FunctionHandler {
+    call(fn, proxy, realThis, argumentsList) {
+      return fn.apply(realThis, argumentsList);
     }
   }
 
   class EventTargetHandler extends ObjectHandler {
-    constructor(target) {
-      super(target);
+    constructor() {
+      super();
       this.listeners = {};
     }
 
     getListeners(event) {
-      if (!this.listeners.hasOwnProperty(event))
-        this.listeners[event] = new Map();
+      if (!this.listeners.hasOwnProperty(event)) this.listeners[event] = new Map();
       return this.listeners[event];
     }
 
     get(target, prop, receiver) {
       if (prop === 'addEventListener') {
-        return new Proxy(
-          target.addEventListener,
-          new this.addEventListener(target.addEventListener)
-        );
+        return new Proxy(target.addEventListener, new this.addEventListener());
       } else if (prop === 'removeEventListener') {
-        return new Proxy(
-          target.removeEventListener,
-          new this.removeEventListener(target.removeEventListener)
-        );
+        return new Proxy(target.removeEventListener, new this.removeEventListener());
       } else return super.get(target, prop, receiver);
     }
   }
 
-  EventTargetHandler.prototype.addEventListener = class extends FunctionHandlerBase {
-    call(fn, proxy, realTarget, argumentsList) {
+  EventTargetHandler.prototype.addEventListener = class extends (
+    FunctionHandler
+  ) {
+    call(fn, proxy, realThis, argumentsList) {
       const event = argumentsList[0];
       const listener = argumentsList[1];
       const bridge = listener.bind(proxy);
       argumentsList[1] = bridge;
       proxy[ProxyGetHandler].getListeners(event).set(listener, bridge);
-      return fn.apply(realTarget, argumentsList);
+      return fn.apply(realThis, argumentsList);
     }
   };
 
-  EventTargetHandler.prototype.removeEventListener = class extends FunctionHandlerBase {
-    call(fn, proxy, realTarget, argumentsList) {
+  EventTargetHandler.prototype.removeEventListener = class extends (
+    FunctionHandler
+  ) {
+    call(fn, proxy, realThis, argumentsList) {
       const event = argumentsList[0];
       const listener = argumentsList[1];
       const cache = proxy[ProxyGetHandler].getListeners(event);
@@ -100,28 +92,16 @@ import { url_status, url_play, url_api_replace, url_replace_to } from './url';
         argumentsList[1] = cache.get(listener);
         cache.delete(listener);
       }
-      return fn.apply(realTarget, argumentsList);
+      return fn.apply(realThis, argumentsList);
     }
   };
 
   class XhrHandler extends EventTargetHandler {
-    constructor(target) {
-      super(target);
-      this.overrideResponse = false;
-      this.overrideResponseValue = null;
-    }
-
     get(target, prop, receiver) {
       if (prop === 'open') {
-        return new Proxy(target.open, new this.open(target.open));
+        return new Proxy(target.open, new this.open());
       } else if (prop === 'send') {
-        return new Proxy(target.send, new this.send(target.send));
-      } else if (prop === 'response' && this.overrideResponse) {
-        console.log('BAL: Return hooked area limit');
-        return this.overrideResponseValue;
-      } else if (prop === 'responseText' && this.overrideResponse) {
-        console.log('BAL: Return hooked area limit');
-        return this.overrideResponseValue;
+        return new Proxy(target.send, new this.send());
       } else {
         return super.get(target, prop, receiver);
       }
@@ -131,11 +111,7 @@ import { url_status, url_play, url_api_replace, url_replace_to } from './url';
   const showTamperMonkeyUpdate = () => {
     GM.getValue('__area__limit__', 0).then((last) => {
       if (last > new Date().getTime() - 86400000) return;
-      if (
-        confirm(
-          'Bilibili　港澳台: 无法获取播放文件信息，如果已开通大会员，请升级油猴到BETA版本'
-        )
-      ) {
+      if (confirm('Bilibili　港澳台: 无法获取播放文件信息，如果已开通大会员，请升级油猴到BETA版本')) {
         window.open(
           'https://chrome.google.com/webstore/detail/tampermonkey-beta/gcalenpjmijncebpfijmoaglllgpjagf',
           '_blank'
@@ -147,8 +123,10 @@ import { url_status, url_play, url_api_replace, url_replace_to } from './url';
   };
 
   let limited = false;
-  XhrHandler.prototype.open = class extends FunctionHandlerBase {
-    call(fn, proxy, realTarget, argumentsList) {
+  XhrHandler.prototype.open = class extends (
+    FunctionHandler
+  ) {
+    call(fn, proxy, realThis, argumentsList) {
       const method = argumentsList[0];
       const url = argumentsList[1];
 
@@ -163,84 +141,156 @@ import { url_status, url_play, url_api_replace, url_replace_to } from './url';
             }
             if (any()) {
               argumentsList[1] = url.replace(url_api_replace, to.api);
-              realTarget.hookCookie = true;
-              console.log(`BAL: playurl via proxy ${to.api}.`);
+              realThis.hookCookie = true;
+              console.log(`BAL: playurl via proxy ${argumentsList[1]}.`);
               break;
             }
           }
-        } else if (
-          (function () {
-            for (const status of url_status) {
-              if (url.match(status)) return true;
-            }
-          })()
-        ) {
-          realTarget.addEventListener('readystatechange', () => {
-            if (realTarget.readyState === 4 && realTarget.status === 200) {
-              const status = JSON.parse(realTarget.response);
-              if (status && status.result && status.result.area_limit === 1) {
-                status.result.area_limit = 0;
-                limited = true;
-                console.log('BAL: Hook area limit');
-                proxy[ProxyGetHandler].overrideResponse = true;
-                proxy[ProxyGetHandler].overrideResponseValue = JSON.stringify(
-                  status
-                );
-              }
-            }
-          });
         }
       }
-      return fn.apply(realTarget, argumentsList);
+      return fn.apply(realThis, argumentsList);
     }
   };
 
-  XhrHandler.prototype.send = class extends FunctionHandlerBase {
-    call(fn, proxy, realTarget, argumentsList) {
-      if (realTarget.hookCookie) {
-        GM_cookie.list(
-          { domain: '.bilibili.com', name: 'SESSDATA' },
-          (cookies, error) => {
-            if (error) {
-              console.log('BAL: Error fetch cookie, not login');
-              realTarget.addEventListener('readystatechange', () => {
-                if (realTarget.readyState === 4 && realTarget.status === 200) {
-                  const status = JSON.parse(realTarget.response);
-                  if (status.code == -10403) showTamperMonkeyUpdate();
-                }
-              });
-              fn.apply(realTarget, argumentsList);
-            } else {
-              console.log(`BAL: Get Cookie ${cookies}`);
-              realTarget.setRequestHeader('X-Cookie', cookies[0].value);
-              fn.apply(realTarget, argumentsList);
-            }
+  XhrHandler.prototype.send = class extends (
+    FunctionHandler
+  ) {
+    call(fn, proxy, realThis, argumentsList) {
+      if (realThis.hookCookie) {
+        GM_cookie.list({ domain: '.bilibili.com', name: 'SESSDATA' }, (cookies, error) => {
+          if (error) {
+            console.log('BAL: Error fetch cookie, not login');
+            realThis.addEventListener('readystatechange', () => {
+              if (realThis.readyState === 4 && realThis.status === 200) {
+                const status = JSON.parse(realThis.response);
+                if (status.code == -10403) showTamperMonkeyUpdate();
+              }
+            });
+            fn.apply(realThis, argumentsList);
+          } else {
+            console.log(`BAL: Get Cookie ${cookies}`);
+            realThis.setRequestHeader('X-Cookie', cookies[0].value);
+            fn.apply(realThis, argumentsList);
           }
-        );
+        });
       } else {
-        fn.apply(realTarget, argumentsList);
+        fn.apply(realThis, argumentsList);
       }
     }
   };
 
-  unsafeWindow.XMLHttpRequest = new Proxy(
-    XMLHttpRequest,
-    new ClassHandler(XhrHandler)
-  );
+  unsafeWindow.XMLHttpRequest = new Proxy(XMLHttpRequest, new ClassHandler(XhrHandler));
 
-  (() => {
-    var info = undefined;
-    Object.defineProperty(unsafeWindow, '__PGC_USERSTATE__', {
-      configurable: true,
-      get: () => info,
-      set: (v) => {
-        if (v.area_limit == 1) {
+  const ProxyIsProxy = Symbol('ProxyIsProxy');
+  class ObjectTreeHandler {
+    constructor(target, subtree) {
+      this.subtree = subtree;
+      for (const key in subtree) {
+        if (key in target) this.set(target, key, target[key]);
+      }
+    }
+
+    get(target, prop, receiver) {
+      if (prop == ProxyIsProxy) {
+        return true;
+      } else {
+        return Reflect.get(target, prop, receiver);
+      }
+    }
+
+    set(target, prop, value, receiver) {
+      if (prop in this.subtree) {
+        const sub = this.subtree[prop];
+        if (typeof sub == 'object') {
+          if (!value[ProxyIsProxy]) {
+            if (Array.isArray(sub)) {
+              return Reflect.set(target, prop, new Proxy(value, new ArrayTreeHandler(value, sub[0])));
+            } else {
+              return Reflect.set(target, prop, new Proxy(value, new ObjectTreeHandler(value, sub)));
+            }
+          } else {
+            return Reflect.set(target, prop, value);
+          }
+        } else if (typeof sub == 'function') {
+          return Reflect.set(target, prop, sub(value));
+        } else {
+          return Reflect.set(target, prop, value);
+        }
+      } else {
+        return Reflect.set(target, prop, value);
+      }
+    }
+  }
+
+  class ArrayTreeHandler {
+    constructor(target, subtree) {
+      this.subtree = subtree;
+      for (let [index, value] of target.entries()) {
+        if (!value[ProxyIsProxy]) target[index] = new Proxy(value, new ObjectTreeHandler(value, subtree));
+      }
+    }
+
+    get(target, prop, receiver) {
+      if (Array.prototype.hasOwnProperty(prop) && !['length', 'slice', 'constructor', 'forEach'].includes(prop))
+        console.log('BAL WARN: ArrayTreeHandler get ' + typeof prop + ':' + prop);
+      return Reflect.get(target, prop, receiver);
+    }
+
+    set(target, prop, value, receiver) {
+      if (!['__proto__'].includes(prop)) console.log('BAL WARN: ArrayTreeHandler set ' + typeof prop + ':' + prop);
+      return Reflect.set(target, prop, value);
+    }
+  }
+
+  const override = {
+    __PGC_USERSTATE__: {
+      area_limit: function (value) {
+        if (value == 1) {
           console.log('BAL: modify area_limit = 0');
           limited = true;
-          v.area_limit = 0;
+          return 0;
         }
-        info = v;
       },
+    },
+    __INITIAL_STATE__: {
+      epInfo: {
+        rights: {
+          area_limit: function (value) {
+            if (value == 1) {
+              console.log('BAL: modify epInfo area_limit = 0');
+              return 0;
+            }
+          },
+        },
+      },
+      epList: [
+        {
+          rights: {
+            area_limit: function (value) {
+              if (value == 1) {
+                console.log('BAL: modify epList area_limit = 0');
+                return 0;
+              }
+            },
+          },
+        },
+      ],
+    },
+  };
+
+  (() => {
+    var userstate = undefined;
+    Object.defineProperty(unsafeWindow, '__PGC_USERSTATE__', {
+      configurable: true,
+      get: () => userstate,
+      set: (v) => (userstate = new Proxy(v, new ObjectTreeHandler(v, override.__PGC_USERSTATE__))),
+    });
+
+    var state = undefined;
+    Object.defineProperty(unsafeWindow, '__INITIAL_STATE__', {
+      configurable: true,
+      get: () => state,
+      set: (v) => (state = new Proxy(v, new ObjectTreeHandler(v, override.__INITIAL_STATE__))),
     });
   })();
 
--- src/url.js
+++ src/url.js
@@ -1,39 +1,43 @@
-export const url_status = [
-  /^https:\/\/bangumi\.bilibili\.com\/view\/web_api\/season\/user\/status\?.*/,
-  /^https:\/\/api\.bilibili\.com\/pgc\/view\/web\/season\/user\/status\?.*/,
-];
+export const url_status = /^(https:)?\/\/api\.bilibili\.com\/pgc\/view\/web\/season\/user\/status\?.*/;
+export const url_play = /^(https:)?\/\/api\.bilibili\.com\/pgc\/player\/web\/playurl\?.*/;
+export const url_season = /^(https:)?\/\/api\.bilibili\.com\/pgc\/view\/web\/season\?.*/;
 
-export const url_play = /^https:\/\/api\.bilibili\.com\/pgc\/player\/web\/playurl\?.*/;
+export const url_api_replace = /^(https:)?\/\/api\.bilibili\.com\//;
 
-export const url_api_replace = /^https:\/\/api\.bilibili\.com\//;
+function build_replace_api(region) {
+  return function (str, schema) {
+    if (schema) return schema + '//bilibili-' + region + '-api.kghost.info/';
+    else return '//bilibili-' + region + '-api.kghost.info/';
+  };
+}
 
 export const url_replace_to = [
   [
     // TW
     [/僅.*台/],
     {
-      api: 'https://bilibili-tw-api.kghost.info/',
+      api: build_replace_api('tw'),
     },
   ],
   [
     // HK
     [/僅.*港/],
     {
-      api: 'https://bilibili-hk-api.kghost.info/',
+      api: build_replace_api('hk'),
     },
   ],
   [
     // SG
     [/仅限东南亚/],
     {
-      api: 'https://bilibili-sg-api.kghost.info/',
+      api: build_replace_api('sg'),
     },
   ],
   [
     // CN
     [/^((?!僅).)*$/],
     {
-      api: 'https://bilibili-cn-api.kghost.info/',
+      api: build_replace_api('cn'),
     },
   ],
 ];
